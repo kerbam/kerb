ba87dd1aea3b2b00a855ea87defdfc998888f85d
Matt Kerbawy <matt@pendo.io>
2022-06-16 13:00:24 -0400

    Tests for xtm

--- src/App.js
+++ src/App.js
@@ -3,19 +3,63 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img usemap="#logomap" src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img usemap="#logomap" src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+                <font color="red">*</font>
+                <label for="customerId">
+                    Customer
+                </label>
+                <select id="customerId" name="customerId" class="search-input" onchange="jspChangeCurrentCustomer()"
+                    ng-non-bindable="true">
+                    <option value="">Choose...</option>
+                    <option ng-non-bindable="true" value="ALL_CUSTOMERS">All</option>
+                    <option ng-non-bindable="true" value="6398">IOSApp</option>
+                    <option ng-non-bindable="true" value="7718">Pendo Test</option>
+                    <option ng-non-bindable="true" value="2051">TestCustomer</option>
+                </select>
+                <font color="red">*</font>
+                <label for="sourceLanguage">
+                    Source language
+                </label>
+                <select name="sourceLanguage" id="sourceLanguage" class="search-input search-input-id"
+                    onchange="jspSrcLanguageChanged(event);" disabled="">
+                    <option value="All">Choose...</option>
+                </select>
+                <img id="unFreezerSrcLang" style="display:none;" src="themes/_default/1/images/pages/smallDelete.gif"
+                    onclick="jspUnFrezeeSrcLang();" onmouseover="showTooltip(event,'Reset source language');return false"
+                    onmouseout="hideTooltip()" />
+                <font color="red">*</font>
+                <label for="targetLanguage">
+                    Target language
+                </label>
+                <span id="targetLanguages" class="edit icon disabled">✎</span>
+                <select name="targetLanguage" id="targetLanguage" class="search-input search-input-id"
+                    onchange="jspTgtLanguageChanged(event);" disabled="">
+                    <option value="">Choose...</option>
+                </select>
+                <img id="unFreezerTgtLang" style="display:none;" src="themes/_default/1/images/pages/smallDelete.gif"
+                    onclick="jspUnFrezeeTgtLang();" onmouseover="showTooltip(event,'Reset target language');return false"
+                    onmouseout="hideTooltip()">
+                    <label for="projectName">
+                        Project
+                    </label>
+                    <select name="projectName" id="projectName" class="search-input" onchange="jspProjectNameChanged(event);" disabled="">
+                        <option value="All" selected="selected">All</option>
+                    </select>
+                    <img id="unFreezerProjects" style="display:none;" src="themes/_default/1/images/pages/smallDelete.gif"
+                        onclick="jspUnFrezeeProjects();" onmouseover="showTooltip(event,'Reset project');return false"
+                        onmouseout="hideTooltip()" /></img>
+            </div>
+        );
+    }
 }
 
 export default App;
